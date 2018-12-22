039008037867d7e234a836c0d02c9abe903cab7a
Vicky <vivekltp120@gmail.com>
2018-12-22 18:15:49 +0530

    dynamic programming kanpsack

--- dynamic.py
+++ dynamic.py
@@ -29,8 +29,48 @@ def longest_common_subsequence(x,y):
                 a[i][j] ='vertical'
 
     getlcs(a,x)
+#find the maximum profit for given weight
+def createDictAndMaximumWeight():
+    wdict=[]
+    infile=open('knapsack.txt','r')
+    for line in infile.readlines():
+     p,w=[int(x) for x in line.split()]
+     wdict.append((p,w))
+    wdict.append((0,0))
+    wdict.sort()
+    print(wdict)
+    for i in range(len(wdict)):
+        print(wdict[i][1])
+    print('Give the max weight for which object will be selected')
+    m=int(input())
+    return wdict,m
+
+def knapsack_problem():
+    wdict,m=createDictAndMaximumWeight()
+    n=len(wdict)
+    profitmatrix=[[0 for x in range(m+1)] for y in range(n)]
+    for i in range(1,n):
+        for w in range(1,m+1):
+            if i-1 >= 0:
+                p=profitmatrix[i-1][w]
+            else :
+                p=0
+
+            if w-wdict[i][1]>=0 and i-1 >= 0:
+                q=profitmatrix[i-1][w-wdict[i][1]]
+            else:
+                q=0
+
+            profitmatrix[i][w]=max(p,q+wdict[i][0])
+
+    print(profitmatrix)
+
+
+
+
 
 
 
 if __name__=='__main__' :
-    longest_common_subsequence('abcde','bd')
+    # longest_common_subsequence('abcde','bd')
+    knapsack_problem()
