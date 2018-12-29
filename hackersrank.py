9572782b701f6fbccbb9b28911d885b58a3c08bd
vivekltp120 <vivekltp120@gmail.com>
2018-12-29 21:00:51 +0530

    leetcode,hackerscode and regex added

--- hackersrank.py
+++ hackersrank.py
@@ -1,27 +1,83 @@
 #find the sum of submatrix
 
-def createMatrix(file,n,m):
-    matrix=[[None for x in range(m)] for y in range(n)]
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the hourglassSum function below.
+def hourglassSum():
+    arr = []
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
     i=0
-    for line in file.readlines():
-        matrix[i]=list(map(int,line.split()))
-        i+=1
-    return matrix
-def sum_of_sum_matrix(matrix,k):
-    print('In sum of matrix'+str(len(matrix)))
-    n=len(matrix)
-    m=len(matrix[0])
-    for i in range(n-k+1):
-        for j in range(m-k+1):
+    j=0
+    hsumarr=[]
+    while True:
+        hsum=0
+        for x in range(i,i+3):
+            for y in range(j,j+3):
+                print((x,y))
+                hsum+=arr[x][y]
 
-            for p in range(i,i+k):
-                for q in range(j,j+k):
-                    sum=matrix[p][q]
-            mid_i=i+int(k/2)
-            sum-=(matrix[mid_i][j]+matrix[mid_i][j+k-1])
-            print(sum)
+        hsum=hsum-arr[(i+i+2)//2][j]-arr[(i+i+2)//2][j+2]
+        hsumarr.append(hsum)
+        j+=1
+        if j>3:
+            i+=1
+            j=0
+        if i>3:
+            break
+    return max(hsumarr)
+#Tokenize the sentences and remove punctuation symbol and convert in lower case
+def tokenizer():
+    list_of_sentences=[]
+    fp=open('tokenizer_input.txt','r')
+    list_of_sentences.extend(fp.readlines())
+    print(list_of_sentences)
+    print(len(list_of_sentences))
+    tokenlist=[]
+    for x in list_of_sentences:
+        x.lower()
+        tokenlist.extend(re.split('.\n',x))
+    print(tokenlist)
 
-if __name__=='__main__':
-    infile=open('mat_input.txt','r')
-    m=createMatrix(infile,6,6)
-    sum_of_sum_matrix(m,3)
+
+# Complete the arrayManipulation function below.
+def arrayManipulation():
+    nm = input().split()
+
+    n = int(nm[0])
+
+    m = int(nm[1])
+
+    queries = []
+
+    for _ in range(m):
+        queries.append(list(map(int, input().rstrip().split())))
+
+
+
+
+    max_val = 0
+    qlist=[]
+    qk={}
+    print(qlist)
+    for i in range(len(queries)):
+        si, ei, kv = list(map(int, queries[i]))
+        temp_set = set([x  for x in range(si, ei + 1)])
+        qlist.append(temp_set)
+        qk.__setitem__(temp_set,kv)
+    for x in qlist:
+        pass
+
+    print (max_val)
+
+if __name__ == '__main__':
+    # result = hourglassSum()
+    # print(result)
+    # tokenizer()
+    arrayManipulation()
--- leetcode.py
+++ leetcode.py
@@ -36,3 +36,23 @@ def sum_of_two_list():
                 sumlist.append(carry)
 
             return sumlist
+
+#leet code remove the duplicate characters
+def removeDuplicates():
+    nums=[1,1,1,2,2,3,3,3,4,4,4]
+    if len(nums) > 0:
+        i=1
+        while True:
+            if nums[i] == nums[i - 1]:
+                nums.pop(i - 1)
+                print(nums)
+            else:
+                i+=1
+            if i==len(nums):
+                break
+        print(len(nums))
+    else:
+        print(len(nums))
+
+removeDuplicates()
+
